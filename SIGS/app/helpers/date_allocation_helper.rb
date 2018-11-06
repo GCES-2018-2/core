822393ad81980798e8b014514743151d3705b62e
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-11-06 16:12:59 -0200

    Fixing allocation_helper functions
    Adding step into method to save allocation
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- SIGS/app/helpers/date_allocation_helper.rb
+++ SIGS/app/helpers/date_allocation_helper.rb
@@ -2,14 +2,12 @@
 
 # Categories module
 module DateAllocationHelper
-  def pass_to_all_allocations_helper(allocation)
-    period = Period.find_by(period_type: 'Letivo')
-    date = period.initial_date
+  def run_all_allocation_date(week_days, allocation, date, period, solicitation)
     while date != period.final_date
       all_allocation_date = AllAllocationDate.new
       all_allocation_date.allocation_id = allocation.id
 
-      %w[Segunda Terça Quarta Quinta Sexta Sabado].each_with_index do |day, index|
+      week_days.each_with_index do |day, index|
         next unless allocation.day == day && date.wday == index + 1
         all_allocation_date.day = date
         all_allocation_date.save
@@ -17,23 +15,31 @@ module DateAllocationHelper
       end
       date += 1
     end
+    if solicitation
+      allocation.save
+    end
+  end
+
+  def run_allocation(allocation, date, period, solicitation)
+    if solicitation
+      week_days = %w[segunda terca quarta quinta sexta sabado]
+      run_all_allocation_date(week_days, allocation, date, period, solicitation)
+    else
+      week_days = %w[Segunda Terça Quarta Quinta Sexta Sabado]
+      run_all_allocation_date(week_days, allocation, date, period, solicitation)
+      allocation.save
+    end
+  end
+
+  def pass_to_all_allocations_helper(allocation)
+    period = Period.find_by(period_type: 'Letivo')
+    date = period.initial_date
+    run_allocation(allocation, date, period, false)
   end
 
   def pass_to_all_solicitations_helper(allocation)
     period = Period.find_by(period_type: 'Letivo')
     date = period.initial_date
-    while date != period.final_date
-      all_allocation_date = AllAllocationDate.new
-      all_allocation_date.allocation_id = allocation.id
-
-      %w[segunda terca quarta quinta sexta sabado].each_with_index do |day, index|
-        next unless allocation.day == day && date.wday == index + 1
-        all_allocation_date.day = date
-        all_allocation_date.save
-        all_allocation_date = nil
-      end
-      date += 1
-    end
-    allocation.save
+    run_allocation(allocation, date, period, true)
   end
 end
