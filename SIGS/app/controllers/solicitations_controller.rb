21814474626714dda776c18b5da07ec90b9538d6
Romeu Carvalho Antunes <romeucarvalho2009@hotmail.com>
2018-11-20 11:54:36 -0200

    Add new methods on solicitations helper

--- SIGS/app/controllers/solicitations_controller.rb
+++ SIGS/app/controllers/solicitations_controller.rb
@@ -112,28 +112,8 @@ class SolicitationsController < ApplicationController
 
   private
 
-  def update_room_status(room_solicitation)
-    room_solicitation.update(justify: params[:justification],
-                             responder_id: current_user,
-                             response_date: Date.today,
-                             status: 2)
-  end
-
-  def department_room(rooms, reservations)
-    avaliable_rooms_hash = []
-    rooms.each do |room|
-      next unless avaliable_room_day(reservations, room)
-      avaliable_rooms_hash.push [room, room.building, room.department, room.category]
-    end
-    avaliable_rooms_hash
-  end
-
   def allocation_period?
     Date.current < Period.find_by(period_type: 'Alocação').final_date
-  end
-
-  def solicitation_params
-    params[:solicitation].permit(:departments, :justify, :school_room_id)
   end
 
   def save(group_solicitation, solicitation)
@@ -147,26 +127,6 @@ class SolicitationsController < ApplicationController
       ocurred_errors(solicitation)
       redirect_to allocation_period_path(solicitation.school_room.id)
     end
-  end
-
-  def save_in_period(solicitation, rooms, group)
-    group.each do |row|
-      row.each do |room_solicitation|
-        start = "#{room_solicitation[:start_time]}:00"
-        final = "#{room_solicitation[:final_time]}:00"
-        i = 0
-        loop do
-          department = params[:solicitation][:departments]
-          department = rooms[i].department_id if department.nil?
-          solicitation.room_solicitation
-                      .build(start: start, final: final, day: room_solicitation[:day],
-                             room: rooms[i], department_id: department)
-          i += 1
-          break unless i < rooms.size
-        end
-      end
-    end
-    save(group, solicitation)
   end
 
   def create_solicitation
--- SIGS/app/helpers/solicitations_helper.rb
+++ SIGS/app/helpers/solicitations_helper.rb
@@ -28,4 +28,44 @@ module SolicitationsHelper
   def pass_to_all_allocation_dates_aux(allocation)
     pass_to_all_solicitations_helper(allocation)
   end
+
+  def update_room_status(room_solicitation)
+    room_solicitation.update(justify: params[:justification],
+                             responder_id: current_user,
+                             response_date: Date.today,
+                             status: 2)
+  end
+
+  def department_room(rooms, reservations)
+    avaliable_rooms_hash = []
+    rooms.each do |room|
+      next unless avaliable_room_day(reservations, room)
+      avaliable_rooms_hash.push [room, room.building, room.department, room.category]
+    end
+    avaliable_rooms_hash
+  end
+
+  def solicitation_params
+    params[:solicitation].permit(:departments, :justify, :school_room_id)
+  end
+
+  def save_in_period(solicitation, rooms, group)
+    group.each do |row|
+      row.each do |room_solicitation|
+        start = "#{room_solicitation[:start_time]}:00"
+        final = "#{room_solicitation[:final_time]}:00"
+        i = 0
+        loop do
+          department = params[:solicitation][:departments]
+          department = rooms[i].department_id if department.nil?
+          solicitation.room_solicitation
+                      .build(start: start, final: final, day: room_solicitation[:day],
+                             room: rooms[i], department_id: department)
+          i += 1
+          break unless i < rooms.size
+        end
+      end
+    end
+    save(group, solicitation)
+  end
 end
