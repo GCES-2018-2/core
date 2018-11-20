1f969b713d4811ab7c9dd4a12900dce73becfa98
Kairon Velozo <kairon333@gmail.com>
2018-11-20 16:28:35 -0200

    Applying DIP for report generation.

--- SIGS/app/controllers/reports_controller.rb
+++ SIGS/app/controllers/reports_controller.rb
@@ -42,22 +42,37 @@ class ReportsController < ApplicationController
   end
 
   def generate_by_room
-    report = Prawn::Document.new(page_size: 'A4', page_layout: :landscape) do |pdf|
-      if params[:reports_by_room][:all_rooms] == '0'
-        room_selected = Room.find(params[:reports_by_room][:room_code])
-        TableRoom.generate_room_page_report(pdf, room_selected)
-      else
-        new_page = false
-        rooms = Room.where(department: params[:reports_by_room][:departments])
-        rooms.each do |room|
-          pdf.start_new_page if new_page
-          TableRoom.generate_room_page_report(pdf, room)
-          new_page = true
-        end
-      end
+    report = Report.new
+    if params[:reports_by_room][:all_rooms] == '0'
+      data = Room.find(params[:reports_by_room][:room_code])
+      formatter = SinglePageReport.new(data)
+      report.format(formatter)
+    else
+      data = Room.where(department: params[:reports_by_room][:departments])
+      formatter = MultiplagePageReport.new(data)
+      report.format(formatter)
     end
-    send_data report.render, type: 'application/pdf', disposition: 'inline'
+    document = report.document
+    send_data document.render, type: 'application/pdf', disposition: 'inline'
   end
+
+  # def generate_by_room
+  #   report = Prawn::Document.new(page_size: 'A4', page_layout: :landscape) do |pdf|
+  #     if params[:reports_by_room][:all_rooms] == '0'
+  #       room_selected = Room.find(params[:reports_by_room][:room_code])
+  #       TableRoom.generate_room_page_report(pdf, room_selected)
+  #     else
+  #       new_page = false
+  #       rooms = Room.where(department: params[:reports_by_room][:departments])
+  #       rooms.each do |room|
+  #         pdf.start_new_page if new_page
+  #         TableRoom.generate_room_page_report(pdf, room)
+  #         new_page = true
+  #       end
+  #     end
+  #   end
+  #   send_data report.render, type: 'application/pdf', disposition: 'inline'
+  # end
 
   def json_of_rooms_by_department
     department_code = params[:department_code]
---
+++ SIGS/app/reports/report.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'prawn/table'
+require 'prawn'
+
+# Cria um documento de relatorio
+class Report
+  def initialize
+    @document = Prawn::Document.new(page_size: 'A4', page_layout: :landscape)
+  end
+
+  def format(formatter: SinglePage.new)
+    formatter.new.format(@document)
+  end
+end
+
+# Classe base para formatacao de um relatorio.
+class BaseFormatter
+  def initialize(data)
+    @data = data
+  end
+end
+
+# Classe para formatacao de relatorios de 1 pagina.
+class SinglePage < BaseFormatter
+  def format(document)
+    doc = document
+    doc do |pdf|
+      TableRoom.generate_room_page_report(pdf, @data)
+    end
+  end
+end
+
+# Classe para formatacao de relatorios de multiplas paginas.
+class MultiplePages < BaseFormatter
+  def format(document)
+    doc = document
+    doc do |pdf|
+      new_page = false
+      @data.each do |room|
+        pdf.start_new_page if new_page
+        TableRoom.generate_room_page_report(pdf, room)
+        new_page = true
+      end
+    end
+  end
+end
