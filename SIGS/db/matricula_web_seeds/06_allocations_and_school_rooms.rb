d0c43c3b7afea8ddebb9088aa333f5b592261893
Victor Moura <victor_cmoura@hotmail.com>
2018-12-02 19:50:51 -0200

    Fixes non creation of allocations fetched from MatriculaWeb

--- SIGS/db/matricula_web_seeds/06_allocations_and_school_rooms.rb
+++ SIGS/db/matricula_web_seeds/06_allocations_and_school_rooms.rb
@@ -33,18 +33,27 @@ bar = ProgressBar.new(@allocations_and_school_rooms.count)
     @room = Room.find_by(:code => allocation_info['codigo_local']) 
     next if @room == nil
 
-    @school_room = SchoolRoom.find_or_create_by(
-        name: @name,
-        discipline_id: @discipline.id,
-        vacancies: @room.capacity,
-        course_ids: [] # Due to MW limitations, it is not possible to link courses to a room
-    )
+    @course = Course.find_or_create_by(code: '0', name: 'Não Disponível', department_id: Department.find_by(:name => 'Prefeitura').id, shift: 1)
+
     
+    @school_room = SchoolRoom.find_by(
+        name: @name,
+        discipline_id: @discipline.id
+    )
+    if @school_room == nil
+        @school_room = SchoolRoom.create(
+            name: @name,
+            discipline_id: @discipline.id,
+            vacancies: @room.capacity,
+            course_ids: [@course.id] # Due to MW limitations, it is not possible to link courses to a room
+        )
+    end
+
     @start_time = allocation_info['hora_inicio']
     @end_time = allocation_info['hora_fim']
     @day = allocation_info['hora_dia']
 
-    Allocation.find_or_create_by(
+    @allocation = Allocation.find_or_create_by(
         user_id: 1,
         room_id:  @room.id,
         school_room_id: @school_room.id,
