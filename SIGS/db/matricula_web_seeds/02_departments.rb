e7d44c94f598b5fa155b837220a1a8e80c71886f
Victor Moura <victor_cmoura@hotmail.com>
2018-12-02 18:09:21 -0200

    Adds campi and departments suggested mw seed code to future developers

--- SIGS/db/matricula_web_seeds/02_departments.rb
+++ SIGS/db/matricula_web_seeds/02_departments.rb
@@ -1,1 +1,16 @@
-# Departments are being created on demand in 03_disciplines due to insuficient information from MatriculaWeb's API
+# # Departments are being created on demand in 03_disciplines.rb due to insuficient information from MatriculaWeb's API
+# # The reuse of the code below is strongly recomended due to avoid reworking
+
+# require 'json'
+# require 'progress_bar'
+
+# @departments_json =  MatriculaWeb::Seeder.departments # The module MatriculaWeb does not contain the .departments method yet
+# puts "Fetching departments..."
+# @departments = JSON.parse(@departments_json)
+
+# bar = ProgressBar.new(@departments.count)
+
+# @departments.each do |department|
+#       bar.increment!
+#     # Insert department object in the database
+# end
