73b60b7342ff523d6795648fe5441aa17121e008
Victor Moura <victor_cmoura@hotmail.com>
2018-12-02 15:54:43 -0200

    Adds request feature to MatriculaWeb's module

--- SIGS/lib/matricula_web.rb
+++ SIGS/lib/matricula_web.rb
@@ -1,15 +1,30 @@
 module MatriculaWeb
+    require 'net/http'
+
     class Requester
-        @@base_url = 'baseurl.com.br'
-        
-        def self.make_request
-            @@base_url
+        @@base_url = 'https://homologaservicos.unb.br'
+        @@use_ssl = true # Set to true if HTTPS and false if HTTP
+        @@limit = 10000 # It was informed that, to 'homologaservicos.unb.br', the 10k limit is sufficient to gather all the information 
+
+        def self.make_request(endpoint, limit=@@limit)
+            @request_url = @@base_url + endpoint + '?limit=' + limit.to_s
+            
+            @uri = URI.parse(@request_url)
+            
+            @request = Net::HTTP::Get.new(@uri.to_s)
+            
+            @response = Net::HTTP.start(@uri.host, @uri.port, :use_ssl => @@use_ssl) do |http|
+                http.request(@request)
+            end
+            
+            @response.body
         end
     end
 
     class Seeder
-        def self.courses
-            Requester.make_request
+        def self.disciplines
+            @endpoint = '/dados/academico/oferta/atual/disciplina'
+            Requester.make_request(@endpoint)
         end
     end
 end
