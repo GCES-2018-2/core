b76db12e2ad610519793fa90ec8b5d16439ea593
Hugo Martins <hugomartins_15@hotmail.com>
2018-12-05 19:25:07 -0200

    Fixing Refact controller class user_controllers

--- SIGS/app/controllers/users_controller.rb
+++ SIGS/app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     return unless permission[:level] != 2
     redirect_to_current_user
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
     redirect_to sign_in_path
     flash[:notice] = 'Solicitação de cadastro efetuado com sucesso!'
   end
-  
+
   def edit
     @user = User.find(params[:id])
     return unless @user.id != current_user.id
