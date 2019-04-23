87121c219cdd5fd90aab51b911df14c3abfbcda5
Nukdown <belchior.emanoel@gmail.com>
2019-04-23 11:17:00 -0300

    Refatoring show method to define name on param
    Refactoring show method to define name on param

--- SIGS/app/controllers/users_controller.rb
+++ SIGS/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   before_action :logged_in?
 
   def show
-    @user = User.find(params[:id])
+    @user = User.friendly.find(params[:id])
     @school_room_count = school_rooms_by_user.count
     @school_rooms_allocated_count = school_rooms_allocated_count
     @periods = Period.all
