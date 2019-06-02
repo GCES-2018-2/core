3d954a2bb8bd2bfa3666f7cd51271f857249f2cf
drianne <driah_as@hotmail.com>
2019-06-01 21:35:01 -0300

    Moving filters' methods to helper

--- app/controllers/allocations_controller.rb
+++ app/controllers/allocations_controller.rb
@@ -9,6 +9,7 @@ class AllocationsController < ApplicationController
   before_action :logged_in?
   before_action :authenticate_coordinator?
   include DateAllocationHelper
+  include AllocationHelper
 
   def new
     @allocations = []
@@ -19,59 +20,7 @@ class AllocationsController < ApplicationController
     @school_room = SchoolRoom.find(params[:school_room_id])
     @coordinator_rooms = current_user.coordinator.course.department.rooms
     filtering_params_allocations
-  end
-
-  def search_capacity_by_coordinator_rooms
-    rooms_capacity =[]
-    range = params[:capacity_filter]
-    if range != ''
-      @coordinator_rooms.each do |room|
-        if range=='0-50' && room.capacity < 50 
-          rooms_capacity << room
-        elsif range=='50-100' && room.capacity >=50 && room.capacity < 100
-          rooms_capacity << room
-        elsif range=='100-150' && room.capacity >=100
-          rooms_capacity << room
-        end
-      end
-    else 
-      rooms_capacity = @main_rooms #Possibilita pegar o vetor original de rooms
-    end
-    rooms_capacity
-  end
-
-  def search_resources_by_coordinator_rooms
-    rooms_resources =[]
-    categories = []
-    resource = params[:resources_filter]    
-    if @coordinator_rooms != nil
-      if resource != ''
-        puts "Recurso solicitado: " 
-        puts resource  
-        puts "Número de salas que pertencem ao coordenador: " 
-        puts @coordinator_rooms.length
-        @coordinator_rooms.each do |room|
-          puts "Recurso da "  + room.name + ":"  
-          room.category.each do |category|
-            categories << category.name
-            puts category.name
-          end
-          if categories.include?resource                         
-            rooms_resources << room        
-            puts "Número de salas que possui o recurso solicitado :" + rooms_resources.length.to_s
-          else
-            puts "Não tem " + resource
-          end
-          categories = []
-        end
-      else
-        rooms_resources = @coordinator_rooms
-      end
-    else
-      rooms_resources = @main_rooms
-    end
-    rooms_resources
-  end
+  end  
 
   def search_capacity
     @coordinator_rooms = search_capacity_by_coordinator_rooms
