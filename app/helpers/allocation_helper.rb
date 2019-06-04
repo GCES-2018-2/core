2a218c68c1c94e1314a2d0ee0e20693f4bb3710d
Lucas S <lucas.soaresouza@gmail.com>
2019-06-03 22:13:18 -0300

    Showing previously allocated schedules
    Co-authored-by: Stéfane Souza <stefanesouza04@gmail.com>

--- app/helpers/allocation_helper.rb
+++ app/helpers/allocation_helper.rb
@@ -2,4 +2,21 @@
 
 # module to allocations
 module AllocationHelper
+
+  def get_school_room_by_day_and_hour(room_id, day, hour)
+    #return unless params[:campus_id].present?
+
+    school_room_data = []
+    allocation = Allocation.where(room_id: room_id, day: day, start_time: hour)
+    if allocation[0] != nil
+      school_room = SchoolRoom.where(id: allocation[0].school_room_id)
+      discipline = Discipline.where(id: school_room[0].discipline_id)
+      school_room_data << discipline[0].name
+      school_room_data << school_room[0].name
+      return school_room_data
+    else
+      return nil
+    end
+  end
+
 end
