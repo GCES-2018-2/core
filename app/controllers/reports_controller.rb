e7f7620a7f6c3e870f57bc8f26d43386b88e6c31
Stefane Souza <stefanesouza04@gmail.com>
2019-06-25 07:22:53 -0300

    Fixing rubocop issues

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -52,11 +52,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  def select_room(pdf)
-    room_selected = Room.find(params[:reports_by_room][:room_code])
-    TableRoom.generate_room_page_report(pdf, room_selected)
-  end
-
   def no_room_select(pdf)
     new_page = false
     rooms = Room.where(department: params[:reports_by_room][:departments])
@@ -64,8 +59,13 @@ class ReportsController < ApplicationController
       pdf.start_new_page if new_page
         TableRoom.generate_room_page_report(pdf, room)
         new_page = true
+      end
     end
   end
+
+  def select_room(pdf)
+    room_selected = Room.find(params[:reports_by_room][:room_code])
+    TableRoom.generate_room_page_report(pdf, room_selected)
 
   def json_of_rooms_by_department
     department_code = params[:department_code]
