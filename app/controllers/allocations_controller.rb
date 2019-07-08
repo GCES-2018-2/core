a70e7e4845ded74c74baee89c7bafef5403bd1da
Adrianne Alves <driah_as@hotmail.com>
2019-07-08 16:53:39 -0300

    Showing just coordinators buildings in filter

--- app/controllers/allocations_controller.rb
+++ app/controllers/allocations_controller.rb
@@ -20,7 +20,9 @@ class AllocationsController < ApplicationController
     end
 
     @school_room = SchoolRoom.find(params[:school_room_id])
-    @buildings = Building.all
+
+    @buildings = get_coordinator_buildings
+
     @campi = Campus.all
     @rooms = Room.all
 
