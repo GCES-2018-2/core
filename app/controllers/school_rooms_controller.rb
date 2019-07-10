6da8e576587b6049638506532592f55a23ae00e7
Maria Luiza Ferreira Assumpção Almeida <mlfaa@hotmail.com>
2019-07-10 20:22:35 -0300

    Adding styles in serach disciplines

--- app/controllers/school_rooms_controller.rb
+++ app/controllers/school_rooms_controller.rb
@@ -41,7 +41,7 @@ class SchoolRoomsController < ApplicationController
     else
       @my_school_rooms = SchoolRoom.all
     end
-    @my_school_rooms = @my_school_rooms.paginate(page: params[:page], per_page: 10)
+    filtering_params
   end
 
   def search_disciplines
