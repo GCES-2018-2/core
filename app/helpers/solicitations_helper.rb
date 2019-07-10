c47b308b7f6134b0f9cc15cc456e0d68d808b0ed
Stefane Souza <stefanesouza04@gmail.com>
2019-07-09 23:00:49 -0300

    Fixing allocation error

--- app/helpers/solicitations_helper.rb
+++ app/helpers/solicitations_helper.rb
@@ -59,36 +59,35 @@ module SolicitationsHelper
   end
 
   def save_in_period(solicitation, rooms, group)
-    group.each do |row|
-      row.each do |room_solicitation|
-        save_in_period_departaments(solicitation, rooms)
-      end
-    end
+    save_in_period_departaments(solicitation, rooms, group)
     save(group, solicitation)
   end
 
-  def save_in_period_departaments(solicitation, rooms)
-    i = 0
-    loop do
-      solicitation_room_build(solicitation, rooms)
-      i += 1
-      break unless i < rooms.size
+  def save_in_period_departaments(solicitation, rooms, group)
+    group.each do |row|
+      row.each do |room_solicitation|
+          solicitation_room_build(room_solicitation, solicitation, rooms)
+        end
+      end
     end
   end
 
-  def solicitation_room_build(solicitation, rooms)
+  def solicitation_room_build(room_solicitation, solicitation, rooms)
     start = "#{room_solicitation[:start_time]}:00"
     final = "#{room_solicitation[:final_time]}:00"
-    
-    department = params[:solicitation][:departments]
-    department = rooms[i].department_id if department.nil?
-    solicitation.room_solicitation
-                .build(
-                  start: start,
-                  final: final,
-                  day: room_solicitation[:day],
-                  room: rooms[i],
-                  department_id: department
-                )
+    i = 0
+    loop do
+      department = params[:solicitation][:departments]
+      department = rooms[i].department_id if department.nil?
+      solicitation.room_solicitation
+                  .build(
+                    start: start,
+                    final: final,
+                    day: room_solicitation[:day],
+                    room: rooms[i],
+                    department_id: department
+                  )
+    i += 1
+    break unless i < rooms.size
   end
 end
