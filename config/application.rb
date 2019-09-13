f9b3d17cc82662962b60c4bb158409774e9c4bb8
Alax Alves <alaxallves@gmail.com>
2019-09-13 14:20:03 -0300

    Adding newest framework defaults

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SIGS
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
