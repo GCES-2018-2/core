e4da9e6b081b7b11a2f119c9ce810e47754c4b86
João Gabriel <joao_salibaa@hotmail.com>
2019-11-25 13:18:58 -0300

    Applying method composit in adminstative_assistants

--- app/controllers/administrative_assistants_controller.rb
+++ app/controllers/administrative_assistants_controller.rb
@@ -4,13 +4,14 @@
 class AdministrativeAssistantsController < ApplicationController
   before_action :logged_in?
   before_action :authenticate_administrative_assistant?
+  include AdministrativeAssistantHelper
 
   def registration_request
     @users = User.where(active: 0)
   end
 
   def enable_registration
-    @user = User.find(params[:id])
+    @user = user_by_id
     if @user.update_attribute(:active, 1)
       flash[:success] = 'Usuário aprovado com sucesso'
     end
@@ -18,7 +19,7 @@ class AdministrativeAssistantsController < ApplicationController
   end
 
   def decline_registration
-    @user = User.find(params[:id])
+    @user = user_by_id
     if @user
       @user.destroy
       flash[:success] = 'Usuário recusado com sucesso'
@@ -27,7 +28,7 @@ class AdministrativeAssistantsController < ApplicationController
   end
 
   def destroy_users
-    @user = User.find(params[:id])
+    @user = user_by_id
     return unless @user
     @user.update_attribute('active', 2)
     redirect_to user_index_path, flash: { success: 'Usuário excluído com sucesso' }
